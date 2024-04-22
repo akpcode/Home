e4506341de3c9d5b720706454ff57019fad9580b
Sanya Moses Adeniyi <54499732+akpcode@users.noreply.github.com>
2024-04-22 14:21:30 +0100

    working on basics and introducing react

---
+++ To-do-app/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <main className="App-header">
+        <header>
+          
+        </header>
+      </main>
+    </div>
+  );
+}
+
+export default App;
